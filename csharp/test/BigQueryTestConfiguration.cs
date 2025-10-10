d72fcefff0cc47e6a00a768614896309c8b184b5
David Coe <>
2025-10-10 12:35:26 -0400

    update tests as well

--- csharp/test/BigQueryTestConfiguration.cs
+++ csharp/test/BigQueryTestConfiguration.cs
@@ -119,6 +119,9 @@ namespace Apache.Arrow.Adbc.Tests.Drivers.BigQuery
         [JsonPropertyName("evaluationKind")]
         public string EvaluationKind { get; set; } = string.Empty;
 
+        [JsonPropertyName("location")]
+        public string? ClientLocation { get; set; }
+
         /// <summary>
         /// How structs should be handled by the ADO.NET client for this environment.
         /// </summary>
