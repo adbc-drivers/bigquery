037f0c45641d1885dbf7f24925781a142bf905c4
Anna Lee <anna.lee@dbtlabs.com>
2025-10-27 19:58:14 -0400

    feat(go): Support setting quota project for connection (#3622)
    Adds an option to the Bigquery driver for configuring the quota project
    ("adbc.bigquery.sql.auth.quota_project") (see [Cloud
    Docs](https://docs.cloud.google.com/docs/quotas/quota-project)), so
    users can specify a different project to bill for query execution.
    
    This was previously only [configurable via environment
    variable](https://docs.cloud.google.com/docs/quotas/set-quota-project#set-project-variable).
    
    ---
    
    Cherry-pick of:
    apache/arrow-adbc@b922a77bf0dae7d57914bf003701e6c193c6fd04
    
    Closes #8.

--- go/bigquery_database.go
+++ go/bigquery_database.go
@@ -49,9 +49,10 @@ type databaseImpl struct {
 	// projectID is the catalog
 	projectID string
 	// datasetID is the schema
-	datasetID string
-	tableID   string
-	location  string
+	datasetID    string
+	tableID      string
+	location     string
+	quotaProject string
 }
 
 func (d *databaseImpl) Open(ctx context.Context) (adbc.Connection, error) {
@@ -72,6 +73,7 @@ func (d *databaseImpl) Open(ctx context.Context) (adbc.Connection, error) {
 		location:                   d.location,
 		resultRecordBufferSize:     defaultQueryResultBufferSize,
 		prefetchConcurrency:        defaultQueryPrefetchConcurrency,
+		quotaProject:               d.quotaProject,
 	}
 
 	err := conn.newClient(ctx)
@@ -101,6 +103,8 @@ func (d *databaseImpl) GetOption(key string) (string, error) {
 		return d.clientSecret, nil
 	case OptionStringAuthRefreshToken:
 		return d.refreshToken, nil
+	case OptionStringAuthQuotaProject:
+		return d.quotaProject, nil
 	case OptionStringLocation:
 		return d.location, nil
 	case OptionStringProjectID:
@@ -163,6 +167,8 @@ func (d *databaseImpl) SetOption(key string, value string) error {
 		d.clientSecret = value
 	case OptionStringAuthRefreshToken:
 		d.refreshToken = value
+	case OptionStringAuthQuotaProject:
+		d.quotaProject = value
 	case OptionStringImpersonateTargetPrincipal:
 		d.impersonateTargetPrincipal = value
 	case OptionStringImpersonateDelegates:
