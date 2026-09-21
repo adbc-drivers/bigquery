c81014319d4d5c57753f97727883d20df11c4705
serramatutu <lucas.paes@dbtlabs.com>
2026-09-21 14:39:59 +0200

    Type conversion

--- go/row_based_iterator.go
+++ go/row_based_iterator.go
@@ -25,13 +25,15 @@ package bigquery
 import (
 	"bytes"
 	"fmt"
-	"log"
+	"math/big"
 	"time"
 
 	"cloud.google.com/go/bigquery"
 	"cloud.google.com/go/civil"
 	"github.com/apache/arrow-go/v18/arrow"
 	"github.com/apache/arrow-go/v18/arrow/array"
+	"github.com/apache/arrow-go/v18/arrow/decimal128"
+	"github.com/apache/arrow-go/v18/arrow/decimal256"
 	"github.com/apache/arrow-go/v18/arrow/ipc"
 	"github.com/apache/arrow-go/v18/arrow/memory"
 	"google.golang.org/api/iterator"
@@ -143,7 +145,7 @@ func (l *RowBasedArrowIterator) SerializedArrowSchema() []byte {
 	for i, field := range l.schema {
 		f, err := buildField(field, 0)
 		if err != nil {
-			log.Fatalf("Error building field %s: %v", field.Name, err)
+			return nil
 		}
 		fields[i] = f
 	}
@@ -182,58 +184,12 @@ func rowsToArrowRecordBatch(schema bigquery.Schema, rows [][]bigquery.Value, all
 	}()
 
 	for _, row := range rows {
+		if len(row) != len(builders) {
+			return nil, fmt.Errorf("[bq] row has %d values but schema has %d columns", len(row), len(builders))
+		}
 		for colIdx, val := range row {
-			if val == nil {
-				builders[colIdx].AppendNull()
-				continue
-			}
-
-			switch builder := builders[colIdx].(type) {
-			case *array.Date32Builder:
-				// BigQuery returns civil.Date for DATE columns.
-				if d, ok := val.(civil.Date); ok {
-					t := time.Date(d.Year, time.Month(d.Month), d.Day, 0, 0, 0, 0, time.UTC)
-					builder.Append(arrow.Date32FromTime(t))
-				} else if t, ok := val.(time.Time); ok {
-					builder.Append(arrow.Date32FromTime(t))
-				} else {
-					builder.AppendNull()
-				}
-			case *array.TimestampBuilder:
-				if ts, ok := val.(time.Time); ok {
-					builder.Append(arrow.Timestamp(ts.UnixMicro()))
-				} else {
-					builder.AppendNull()
-				}
-			case *array.Int8Builder:
-				if v, ok := val.(int8); ok {
-					builder.Append(v)
-				} else {
-					builder.AppendNull()
-				}
-			case *array.Int16Builder:
-				if v, ok := val.(int16); ok {
-					builder.Append(v)
-				} else {
-					builder.AppendNull()
-				}
-			case *array.Int32Builder:
-				if v, ok := val.(int32); ok {
-					builder.Append(v)
-				} else {
-					builder.AppendNull()
-				}
-			case *array.Int64Builder:
-				if v, ok := val.(int64); ok {
-					builder.Append(v)
-				} else {
-					builder.AppendNull()
-				}
-			// TODO: Add support for other types as needed. The Storage-API-
-			// disabled path is intended for pseudo-column queries which are
-			// almost always DATE/TIMESTAMP; extend here if more types come up.
-			default:
-				return nil, fmt.Errorf("storage API is disabled, unsupported type conversion for column type %s of value %v", builder.Type().String(), val)
+			if err := appendRowValue(builders[colIdx], val); err != nil {
+				return nil, err
 			}
 		}
 	}
@@ -242,6 +198,168 @@ func rowsToArrowRecordBatch(schema bigquery.Schema, rows [][]bigquery.Value, all
 	for i, b := range builders {
 		arrays[i] = b.NewArray()
 	}
+	defer func() {
+		for _, a := range arrays {
+			a.Release()
+		}
+	}()
 
 	return array.NewRecordBatch(arrowSchema, arrays, int64(len(rows))), nil
+}
+
+func appendRowValue(builder array.Builder, val bigquery.Value) error {
+	if lb, ok := builder.(*array.ListBuilder); ok {
+		items, isList := val.([]bigquery.Value)
+		if val != nil && !isList {
+			return unsupportedRowValue(builder, val)
+		}
+		lb.Append(true)
+		for _, item := range items {
+			if err := appendRowValue(lb.ValueBuilder(), item); err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+
+	if val == nil {
+		builder.AppendNull()
+		return nil
+	}
+
+	switch b := builder.(type) {
+	case *array.BooleanBuilder:
+		v, ok := val.(bool)
+		if !ok {
+			return unsupportedRowValue(builder, val)
+		}
+		b.Append(v)
+	case *array.Int64Builder:
+		v, ok := val.(int64)
+		if !ok {
+			return unsupportedRowValue(builder, val)
+		}
+		b.Append(v)
+	case *array.Float64Builder:
+		v, ok := val.(float64)
+		if !ok {
+			return unsupportedRowValue(builder, val)
+		}
+		b.Append(v)
+	case *array.StringBuilder:
+		v, ok := val.(string)
+		if !ok {
+			return unsupportedRowValue(builder, val)
+		}
+		b.Append(v)
+	case *array.BinaryBuilder:
+		v, ok := val.([]byte)
+		if !ok {
+			return unsupportedRowValue(builder, val)
+		}
+		b.Append(v)
+	case *array.Date32Builder:
+		switch v := val.(type) {
+		case civil.Date:
+			b.Append(arrow.Date32FromTime(v.In(time.UTC)))
+		case time.Time:
+			b.Append(arrow.Date32FromTime(v))
+		default:
+			return unsupportedRowValue(builder, val)
+		}
+	case *array.Time64Builder:
+		v, ok := val.(civil.Time)
+		if !ok {
+			return unsupportedRowValue(builder, val)
+		}
+		micros := int64(v.Hour)*int64(time.Hour/time.Microsecond) +
+			int64(v.Minute)*int64(time.Minute/time.Microsecond) +
+			int64(v.Second)*int64(time.Second/time.Microsecond) +
+			int64(v.Nanosecond)/int64(time.Microsecond)
+		b.Append(arrow.Time64(micros))
+	case *array.TimestampBuilder:
+		switch v := val.(type) {
+		case time.Time:
+			b.Append(arrow.Timestamp(v.UnixMicro()))
+		case civil.DateTime:
+			b.Append(arrow.Timestamp(v.In(time.UTC).UnixMicro()))
+		default:
+			return unsupportedRowValue(builder, val)
+		}
+	case *array.Decimal128Builder:
+		v, ok := val.(*big.Rat)
+		if !ok {
+			return unsupportedRowValue(builder, val)
+		}
+		unscaled, err := unscaledRat(v, b.Type().(*arrow.Decimal128Type).Scale)
+		if err != nil {
+			return err
+		}
+		b.Append(decimal128.FromBigInt(unscaled))
+	case *array.Decimal256Builder:
+		v, ok := val.(*big.Rat)
+		if !ok {
+			return unsupportedRowValue(builder, val)
+		}
+		unscaled, err := unscaledRat(v, b.Type().(*arrow.Decimal256Type).Scale)
+		if err != nil {
+			return err
+		}
+		b.Append(decimal256.FromBigInt(unscaled))
+	case *array.MonthDayNanoIntervalBuilder:
+		v, ok := val.(*bigquery.IntervalValue)
+		if !ok {
+			return unsupportedRowValue(builder, val)
+		}
+		b.Append(arrow.MonthDayNanoInterval{
+			Months: v.Years*12 + v.Months,
+			Days:   v.Days,
+			Nanoseconds: int64(v.Hours)*int64(time.Hour) +
+				int64(v.Minutes)*int64(time.Minute) +
+				int64(v.Seconds)*int64(time.Second) +
+				int64(v.SubSecondNanos),
+		})
+	case *array.StructBuilder:
+		switch v := val.(type) {
+		case []bigquery.Value:
+			if len(v) != b.NumField() {
+				return fmt.Errorf("[bq] record has %d values but struct has %d fields", len(v), b.NumField())
+			}
+			b.Append(true)
+			for i, field := range v {
+				if err := appendRowValue(b.FieldBuilder(i), field); err != nil {
+					return err
+				}
+			}
+		case *bigquery.RangeValue:
+			if b.NumField() != 2 {
+				return fmt.Errorf("[bq] range has %d fields, expected 2", b.NumField())
+			}
+			b.Append(true)
+			if err := appendRowValue(b.FieldBuilder(0), v.Start); err != nil {
+				return err
+			}
+			if err := appendRowValue(b.FieldBuilder(1), v.End); err != nil {
+				return err
+			}
+		default:
+			return unsupportedRowValue(builder, val)
+		}
+	default:
+		return unsupportedRowValue(builder, val)
+	}
+	return nil
+}
+
+func unscaledRat(r *big.Rat, scale int32) (*big.Int, error) {
+	pow := new(big.Int).Exp(big.NewInt(10), big.NewInt(int64(scale)), nil)
+	quo, rem := new(big.Int).QuoRem(new(big.Int).Mul(r.Num(), pow), r.Denom(), new(big.Int))
+	if rem.Sign() != 0 {
+		return nil, fmt.Errorf("[bq] cannot represent %s with scale %d", r.RatString(), scale)
+	}
+	return quo, nil
+}
+
+func unsupportedRowValue(builder array.Builder, val bigquery.Value) error {
+	return fmt.Errorf("storage API is disabled, unsupported type conversion for column type %s of value %v", builder.Type().String(), val)
 }
